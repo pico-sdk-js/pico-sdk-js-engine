0ae5de1f09cfc6c0a6039b549db8c510255a4c5c
jt000 <jt000@users.noreply.github.com>
2024-09-07 17:07:27 -0400

    Adding beachball versioning

---
+++ beachball.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    publish: false,
+    push: false, // TEMP FOR TESTING
+    branch: 'origin/main',
+    changehint: 'Don\'t forget to run "npm run beachball:change" to create a change file'
+};
