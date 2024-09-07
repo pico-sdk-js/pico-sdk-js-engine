e4cc1906226312988e8ca00cf8ef4f236ca365f7
jt000 <jt000@users.noreply.github.com>
2024-09-07 17:29:55 -0400

    Updating change description and reorganizing action

--- beachball.config.js
+++ beachball.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publish: false,
-    push: false, // TEMP FOR TESTING
     branch: 'origin/main',
-    changehint: 'Don\'t forget to run "npm run beachball:change" to create a change file'
+    changehint: 'Don\'t forget to run "npm run beachball:change" to create a change file',
+    generateChangelog: 'md'
 };
