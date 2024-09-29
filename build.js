83983bfda0d90fb67166091929fcc7773395476f
Jason Tremper <jt000@users.noreply.github.com>
2024-09-29 11:56:51 -0400

    Adding releaseautomation (#5)
    Adding workflow job for creating and publishing a release

--- build.js
+++ build.js
@@ -11,9 +11,9 @@ const pico_modules = require("./script/build_pico_modules");
 const package = require("./package.json")
 const productName = `${package.name}-${package.version}`;
 
-if (process.env.GITHUB_ENV) {
-  fs.appendFileSync(process.env.GITHUB_ENV, `PACKAGE_VERSION=${package.version}\r\nPRODUCT_NAME=${productName}\r\n`);
-  fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `# ${package.name} ${package.version}\r\n`);
+if (process.env.GITHUB_OUTPUT) {
+  fs.appendFileSync(process.env.GITHUB_OUTPUT, `PACKAGE_NAME=${package.name}\nPACKAGE_VERSION=${package.version}\nPRODUCT_NAME=${productName}\n`);
+  fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `# ${package.name} ${package.version}\n`);
 }
 
 // Parse options
